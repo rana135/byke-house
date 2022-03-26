e43c20e151b3903bb98117d81797b31c0d4a52c8
rana135 <rana286090@gmail.com>
2022-03-26 20:15:51 +0600

    done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Cart.css'
-import ReactModal from '../ReactModal/ReactModal';
+
 
 const Cart = ({chooseOneFromCart,cart, removeAllFromCart}) => {
     return (
@@ -8,8 +8,7 @@ const Cart = ({chooseOneFromCart,cart, removeAllFromCart}) => {
             {
                 cart.map(card => <li>{card.name}</li>)
             }
-            {/* <ReactModal></ReactModal><br></br> */}
-            <button onClick={chooseOneFromCart}>CHOOSE 1 FOR ME</button><br></br>
+            <button onClick={chooseOneFromCart} className='selected-btn'>CHOOSE 1 FOR ME</button><br></br>
             <button onClick={removeAllFromCart} className='selected-btn'>CHOOSE AGAIN</button><br></br>
         </div>
     );
